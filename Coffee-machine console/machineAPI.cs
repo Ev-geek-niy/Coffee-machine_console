cd214f5a7e64d54dc8356551949c8aa2e2527f86
Ev-geek-niy <ev_geek_niy@vk.com>
2022-10-12 09:55:07 +0500

    Исправлено отображение количества ресурсов

--- Coffee-machine console/machineAPI.cs
+++ Coffee-machine console/machineAPI.cs
@@ -251,7 +251,7 @@ public static class machineAPI
 
             int value = _db.GetResource(resourceName);
             string resType = GetResType(resourceName);
-            double convertedValue = resourceName == "cup" ? value : value / 100.0;
+            double convertedValue = resourceName == "cup" ? value : value / 1000.0;
             if (convertedValue != 0)
                 Console.WriteLine($"Количество {resourceName}: {convertedValue} {resType}");
             else
